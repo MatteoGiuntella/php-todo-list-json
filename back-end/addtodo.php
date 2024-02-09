56e3686b7b1506688c7bbbc2bbbca9013e0334c4
MatteoGiuntella <matteo.giuntella@virgilio.it>
2024-02-09 16:39:57 +0100

    esercizio finito

---
+++ back-end/addtodo.php
@@ -0,0 +1,28 @@
+<?php
+
+$allListJson = file_get_contents('db.json');
+// questo passaggio mi serve per mantenere l'array iniziale per fare in modo che non perdo i dati già inseriti(importante)
+
+$allList = json_decode($allListJson, true);
+// traformo il nuovo offetto json con il decode in dìstringa facendolo diventare un array, il true mi serve per farlo diventare array associativo se metto false rimane oggetto normale
+
+$newList =[
+    'lista' => $_POST['lista'],
+    'fatto' => false
+];
+
+$allList[] = $newList;
+
+// creo e aggiungo il nuovo oggetto da inserire nell'array con il push
+
+$newListJson = json_encode($allList);
+// ritrasformo il nuovo array in un json in modo da poterlo inserire direttamente nel mio array json
+
+file_put_contents('db.json', $newListJson);
+// ora lo pusho nel json, il primo argomento è dove voglio metterlo il secondo è cosa voglio scrivere
+
+echo json_encode([
+    'messaggio' => 'aggiunto',
+    'code' => 200
+]);
+// se la procedura è andata a buon fine invio tramite echo un messaggio ok con codice 200(codice buono.....codice 400 errore)
--- front-end/vue.js
+++ front-end/vue.js
@@ -4,10 +4,12 @@
   createApp({
     data() {
       return {
-        myList: []
+        myList: [],
+        newList : '',
       }
       
     },
+
     mounted() {
       console.log('Mounted')
       axios
@@ -16,6 +18,39 @@
               this.myList = res.data
               console.log(this.myList)
           });
+    },
+    methods:{
+      addlist(){
+        
+        axios
+        .post('http://localhost/php-todo-list-json/back-end/addtodo.php',
+        {
+          lista: this.newList,
+          fatto: false
+        },
+        {
+          headers : { 'Content-Type' : 'multipart/form-data'}
+        })
+        .then((res) => {
+            if (res.data.code == 200) {
+              this.myList.push({
+                lista: this.newList,
+                fatto: false
+      
+              })
+              this.newList =  '';
+            }
+        });
+
+      },
+      checked(){
+        if (this.myList.fatto == false) {
+          this.myList.fatto = true
+        }
+        else{
+
+        }
+      }
     }
     
   }).mount('#app')
